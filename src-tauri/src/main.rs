d8d14e55f0bb6493eb23404c7981de624f9ed553
charan k <charancr1630@gmail.com>
2023-01-09 17:50:37 +0530

    working pdf viewer [ui]

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+// use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 // #[tauri::command]
 // fn greet(name: &str) -> String {
@@ -11,16 +11,16 @@ use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 
 fn main() {
-    let quit=CustomMenuItem::new("quit".to_string(),"Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let menu = Menu::new()
-    .add_native_item(MenuItem::Copy)
-    .add_item(CustomMenuItem::new("hide", "Hide"))
-    .add_submenu(submenu);
+    // let quit=CustomMenuItem::new("quit".to_string(),"Quit");
+    // let close = CustomMenuItem::new("close".to_string(), "Close");
+    // let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    // let menu = Menu::new()
+    // .add_native_item(MenuItem::Copy)
+    // .add_item(CustomMenuItem::new("hide", "Hide"))
+    // .add_submenu(submenu);
 
     tauri::Builder::default()
-        .menu(menu)
+        // .menu(menu)
         // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -24,20 +24,19 @@ import * as pdfjs from 'pdfjs-dist';
 import {listen} from '@tauri-apps/api/event';
 import {convertFileSrc} from '@tauri-apps/api/tauri'
 
+var convertedUrl:string;
+
 listen('tauri://file-drop',event=>{
   let url=(event.payload).toString();
   console.log(url);
-  let convertedUrl= convertFileSrc(url);
+  convertedUrl= convertFileSrc(url);
   console.log(convertedUrl);
-})
-
-// Get the canvas element
-const canvas = document.getElementById('pdf');
+  const canvas = document.getElementById('pdf');
 pdfjs.GlobalWorkerOptions.workerSrc = "../node_modules/pdfjs-dist/build/pdf.worker.js";
-// Load the PDF
-pdfjs.getDocument('src\\Rise_of_Augmented_Reality_Current_and_Future_Appli.pdf').promise.then((pdf) => {
+// Load the PDF "C:\\Users\\charan\\Downloads\\Juluri_Akhil_Kumar_Resume.pdf"
+pdfjs.getDocument(convertedUrl).promise.then((pdf) => {
   // Render the PDF on the canvas
-  pdf.getPage(2).then((page) => {
+  pdf.getPage(1).then((page) => {
     const viewport = page.getViewport({scale: 1});
     const context = canvas.getContext('2d');
     canvas.height = viewport.height;
@@ -48,6 +47,28 @@ pdfjs.getDocument('src\\Rise_of_Augmented_Reality_Current_and_Future_Appli.pdf')
     });
   });
 });
+})
+
+let file=document.getElementById("fileinput")?.addEventListener("change",(event)=>{
+  var input=event.target;
+  var file=input.files[0];
+  console.log(event);
+  console.log(file);
+  var reader=new FileReader();
+  reader.onload=()=>{
+    var data=reader.result;
+    console.log(data);
+    // Get the canvas element
+
+}
+});
+
+
+
+
+
+
+
 
 
 
