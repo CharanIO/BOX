9ba87a0114ba7830054878d6520a3b2153143d08
charan k <charancr1630@gmail.com>
2023-01-11 22:29:55 +0530

    added pdftoimg conversion

--- src/main.ts
+++ src/main.ts
@@ -25,29 +25,42 @@ import {listen} from '@tauri-apps/api/event';
 import {convertFileSrc} from '@tauri-apps/api/tauri'
 
 var convertedUrl:string;
-
+var data:[];
 listen('tauri://file-drop',event=>{
   let url=(event.payload).toString();
   console.log(url);
   convertedUrl= convertFileSrc(url);
   console.log(convertedUrl);
+
   const canvas = document.getElementById('pdf');
 pdfjs.GlobalWorkerOptions.workerSrc = "../node_modules/pdfjs-dist/build/pdf.worker.js";
 // Load the PDF "C:\\Users\\charan\\Downloads\\Juluri_Akhil_Kumar_Resume.pdf"
 pdfjs.getDocument(convertedUrl).promise.then((pdf) => {
   // Render the PDF on the canvas
   pdf.getPage(1).then((page) => {
-    const viewport = page.getViewport({scale: 1});
+    const viewport = page.getViewport({scale: 0.5});
     const context = canvas.getContext('2d');
     canvas.height = viewport.height;
     canvas.width = viewport.width;
-    page.render({
+    var rendertask=page.render({
       canvasContext: context,
       viewport: viewport
     });
+    rendertask.promise.then(()=>{
+      var img=document.createElement("img");
+      img.setAttribute('src',canvas.toDataURL('image/png'));
+      document.body.appendChild(img);
+
+     console.log(canvas.toDataURL('image/png'));
+     
+    });
   });
 });
-})
+});
+
+
+
+
 
 let file=document.getElementById("fileinput")?.addEventListener("change",(event)=>{
   var input=event.target;
